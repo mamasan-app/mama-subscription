dc4b03d7a04e38e6a1eccb4b6adc1da30f6a79c5
moisesliotawork <moises.liotawork@gmail.com>
2024-08-30 21:45:57 -0400

    Model Creation: Store, Service and Subscription and Table Creation: stores, services and subscriptions :sparkles:

---
+++ database/migrations/2024_08_31_013543_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('variant')->default('monthly');
+            $table->unsignedInteger('price_cents')->default(0);
+            $table->boolean('published')->default(false);
+            $table->boolean('featured')->default(false);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
---
+++ database/migrations/2024_08_31_013619_create_subscriptions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('status');
+            $table->string('billing_provider')->default('internal');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('renews_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamp('last_notification_at')->nullable();
+
+            $table->foreignId('user_id');
+            $table->foreignId('service_id');
+
+            $table->index('user_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
