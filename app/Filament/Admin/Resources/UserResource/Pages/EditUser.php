f71980722a39d75ef3d1f6cdc0cbf64896f88406
moisesliotawork <moises.liotawork@gmail.com>
2024-09-02 19:48:57 -0400

    Fixed a bug in creating users from the admin panel :bug:

--- app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
@@ -16,4 +16,29 @@ class EditUser extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Si se está actualizando la contraseña, hashearla
+        if (!empty($data['new_password'])) {
+            $data['password'] = bcrypt($data['new_password']);
+        }
+
+        // Guarda el rol en una propiedad temporal para usarlo después de guardar
+        $this->role = $data['role'];
+
+        // Elimina los campos 'role', 'new_password' y 'new_password_confirmation' para que no intenten guardarse directamente en la base de datos
+        unset($data['role']);
+        unset($data['new_password']);
+        unset($data['new_password_confirmation']);
+
+        return $data;
+    }
+
+    protected function afterSave(): void
+    {
+        // Asignar el rol basado en la selección del formulario
+        $this->record->syncRoles($this->role);
+    }
 }
+
