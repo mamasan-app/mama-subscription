66febc9f14c5ab6eb9c993d722d843ec2acd45e5
moisesliotawork <moises.liotawork@gmail.com>
2025-01-06 19:25:02 -0400

    update style in create payment page :sparkles:

--- resources/views/filament/pages/subscription-payment.blade.php
+++ resources/views/filament/pages/subscription-payment.blade.php
@@ -1,6 +1,9 @@
 <x-filament::page>
     <form wire:submit.prevent="submit">
+        <!-- Renderiza el formulario -->
         {{ $this->form }}
+
+        <!-- Botón de envío con margen superior -->
         <div class="mt-4">
             <x-filament::button type="submit">
                 Procesar Pago
