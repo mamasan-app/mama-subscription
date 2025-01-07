a09d280adc6ccf95bf8cc4a7fc14685fec123cff
moisesliotawork <moises.liotawork@gmail.com>
2025-01-07 16:57:27 -0400

    update

---
+++ database/migrations/2025_01_07_205549_update_role_default_in_store_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('store_user', function (Blueprint $table) {
+            // Establecer un valor predeterminado para el campo 'role'
+            $table->string('role')->default('employee')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('store_user', function (Blueprint $table) {
+            // Revertir el valor predeterminado del campo 'role'
+            $table->string('role')->default(null)->change();
+        });
+    }
+};
+
