967ad0e6f05e8c840ead0c4423f0bf10615076b8
moisesliotawork <moises.liotawork@gmail.com>
2025-01-08 17:10:44 -0400

    update edit employee in panel store :bug:

--- app/Filament/Store/Resources/EmployeeResource/Pages/EditEmployee.php
+++ app/Filament/Store/Resources/EmployeeResource/Pages/EditEmployee.php
@@ -3,8 +3,8 @@
 namespace App\Filament\Store\Resources\EmployeeResource\Pages;
 
 use App\Filament\Store\Resources\EmployeeResource;
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Actions;
 
 class EditEmployee extends EditRecord
 {
@@ -16,4 +16,29 @@ class EditEmployee extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        // Separar el prefijo y el número de la cédula
+        if (!empty($data['identity_document'])) {
+            [$data['identity_prefix'], $data['identity_number']] = explode('-', $data['identity_document']);
+        }
+
+        // Prellenar las tiendas seleccionadas
+        $data['stores'] = $this->record->stores()->pluck('id')->toArray();
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Combinar el prefijo y el número de la cédula al guardar
+        $data['identity_document'] = $data['identity_prefix'] . '-' . $data['identity_number'];
+
+        // Eliminar los campos separados para que no causen errores al guardar
+        unset($data['identity_prefix'], $data['identity_number']);
+
+        return $data;
+    }
 }
+
