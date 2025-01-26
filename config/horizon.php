8a9a5732fb17a668c835cd0f447ec06ff2ab8c59
moisesliotawork <moises.liotawork@gmail.com>
2025-01-26 13:23:48 -0400

    update config horizon :bug:

--- config/horizon.php
+++ config/horizon.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'middleware' => [],
+    'middleware' => ['web'],
 
     /*
     |--------------------------------------------------------------------------
@@ -201,6 +201,7 @@ return [
                 'maxProcesses' => 10,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
+                'force' => true,
             ],
         ],
 
