f34b92d1a2b561ccbd05e86a730f1af49dd48012
moisesliotawork <moises.liotawork@gmail.com>
2025-01-26 22:41:27 -0400

    update :bug:

--- app/Jobs/MonitorTransactionStatus.php
+++ app/Jobs/MonitorTransactionStatus.php
@@ -98,9 +98,9 @@ class MonitorTransactionStatus implements ShouldQueue
                             $subscription->update([
                                 'status' => SubscriptionStatusEnum::Active,
                                 'trial_ends_at' => $currentDate->toDateString(), // Finaliza el periodo de prueba
-                                'renews_at' => $currentDate->toDateString(),
-                                'expires_at' => $currentDate->toDateString(),
-                                'ends_at' => $currentDate->toDateString(),
+                                'renews_at' => $renewDate,
+                                'expires_at' => $expireDate,
+                                'ends_at' => $endDate,
                             ]);
 
                             Notification::make()
