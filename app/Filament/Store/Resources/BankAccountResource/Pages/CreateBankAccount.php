80d416c2c60388e6e6f95bad34cd11399b221c31
moisesliotawork <moises.liotawork@gmail.com>
2025-02-05 08:25:56 -0400

    update bank account resource in store panel :bug:

--- app/Filament/Store/Resources/BankAccountResource/Pages/CreateBankAccount.php
+++ app/Filament/Store/Resources/BankAccountResource/Pages/CreateBankAccount.php
@@ -4,6 +4,7 @@ namespace App\Filament\Store\Resources\BankAccountResource\Pages;
 
 use App\Filament\Store\Resources\BankAccountResource;
 use App\Models\BankAccount;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBankAccount extends CreateRecord
@@ -12,12 +13,22 @@ class CreateBankAccount extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // Verificar si se está marcando esta cuenta como predeterminada
-        if (!empty($data['default_account']) && $data['default_account'] == true) {
-            // Desmarcar cualquier otra cuenta predeterminada asociada a la misma tienda
-            BankAccount::where('store_id', $data['store_id'])
-                ->where('id', '!=', $this->record->id ?? null)
-                ->update(['default_account' => false]);
+        // Obtener la tienda en sesión
+        $currentStore = Filament::getTenant();
+
+        if ($currentStore) {
+            // Asignar el store_id a la cuenta bancaria
+            $data['store_id'] = $currentStore->id;
+
+            // Asignar el user_id al propietario de la tienda
+            $data['user_id'] = $currentStore->owner_id;
+
+            // Verificar si se está marcando esta cuenta como predeterminada
+            if (!empty($data['default_account']) && $data['default_account'] == true) {
+                // Desmarcar cualquier otra cuenta predeterminada asociada a la misma tienda
+                BankAccount::where('store_id', $currentStore->id)
+                    ->update(['default_account' => false]);
+            }
         }
 
         return $data;
