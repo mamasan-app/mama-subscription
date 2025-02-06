ff1372d60b180c3b3cd3bbbc12ade30b527e1bcf
Ronald Pereira <rpereira@taecontrol.com>
2025-02-06 16:46:40 -0400

    fix: app.tsx working

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,22 +1,21 @@
 import './bootstrap';
 import '../css/app.css';
- 
+
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
- 
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
- 
+
 const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true });
- 
+
 createInertiaApp({
-    title: (title) => ${title ? ${title} -  : ''}${appName},
-    resolve: (name) => pages[`./Pages/${name}.tsx`],
-    setup({ el, App, props }) {
-        const root = createRoot(el);
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => `${title ? `${title} - ` : ''}${appName}`,
+  resolve: (name) => pages[`./Pages/${name}.tsx`],
+  setup({ el, App, props }) {
+    const root = createRoot(el);
+    root.render(<App {...props} />);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
