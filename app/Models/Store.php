61bff08135cdb254dd7249a9b20d962fafbfd476
moisesliotawork <moises.liotawork@gmail.com>
2025-02-12 14:37:45 -0400

    update error :bug:

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -122,6 +122,6 @@ class Store extends Model
      */
     public function getDefaultBankAccount()
     {
-        return $this->bankAccounts()->where('default_accounts', true)->first();
+        return $this->bankAccounts()->where('default_account', true)->first();
     }
 }
