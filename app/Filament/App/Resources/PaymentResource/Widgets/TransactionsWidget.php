9e58234d55fd47a434dd9f375c1e589671c6a229
moisesliotawork <moises.liotawork@gmail.com>
2025-02-16 22:10:21 -0400

    update transactions widget in panel app :bug:

--- app/Filament/App/Resources/PaymentResource/Widgets/TransactionsWidget.php
+++ app/Filament/App/Resources/PaymentResource/Widgets/TransactionsWidget.php
@@ -6,6 +6,7 @@ use App\Models\Transaction;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use App\Enums\TransactionTypeEnum;
 
 class TransactionsWidget extends BaseWidget
 {
@@ -35,14 +36,14 @@ class TransactionsWidget extends BaseWidget
 
                 Tables\Columns\TextColumn::make('status')
                     ->label('Estado')
-                    ->formatStateUsing(fn ($record) => $record->status->getLabel())
+                    ->formatStateUsing(fn($record) => $record->status->getLabel())
                     ->badge()
-                    ->color(fn ($record) => $record->status->getColor())
+                    ->color(fn($record) => $record->status->getColor())
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('amount')
                     ->label('Monto (USD)')
-                    ->formatStateUsing(fn ($state) => number_format($state, 2).'USD')
+                    ->formatStateUsing(fn($state) => number_format($state, 2) . 'USD')
                     ->sortable(),
 
                 Tables\Columns\TextColumn::make('date')
@@ -54,15 +55,16 @@ class TransactionsWidget extends BaseWidget
             ->actions([
                 Tables\Actions\Action::make('view')
                     ->label('Ver')
-                    ->url(fn ($record) => route('filament.app.resources.transactions.view', ['record' => $record->id]))
+                    ->url(fn($record) => route('filament.app.resources.transactions.view', ['record' => $record->id]))
                     ->icon('heroicon-o-eye'),
             ]);
     }
 
     protected function getQuery()
     {
-        // Recupera todas las transacciones relacionadas con el pago actual
+        // Recupera todas las transacciones relacionadas con el pago actual y que sean de tipo "subscription"
         return Transaction::query()
-            ->where('payment_id', $this->record->id);
+            ->where('payment_id', $this->record->id)
+            ->where('type', TransactionTypeEnum::Subscription->value); // Filtrar solo suscripciones
     }
 }
