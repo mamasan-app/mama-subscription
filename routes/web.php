ea36a6ad6434e09937f1f054bfa70e1d5a641784
moisesliotawork <moises.liotawork@gmail.com>
2025-03-19 18:55:16 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -4,38 +4,21 @@ use App\Http\Controllers\MagicLinkLoginController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-//    $panel = request('panel', 'app'); // Obtén el panel desde la URL, predeterminado a 'app'
-//
-//    $request->fulfill();
-//
-//    // Redirige al panel correspondiente
-//    switch ($panel) {
-//        case 'tienda':
-//            return redirect('/tienda');
-//        case 'admin':
-//            return redirect('/admin');
-//        default:
-//            return redirect('/app');
-//    }
-//})->middleware(['auth', 'signed'])->name('verification.verify');
-
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $panel = request('panel', 'app'); // Si no se pasa, se asume que va a 'app'
+    $panel = request('panel', 'app'); // Obtén el panel desde la URL, predeterminado a 'app'
 
     $request->fulfill();
 
-    // Redirigir al panel correcto
+    // Redirige al panel correspondiente
     switch ($panel) {
         case 'tienda':
             return redirect('/tienda');
         case 'admin':
             return redirect('/admin');
         default:
-            return redirect('/app'); // Redirige siempre a /app
+            return redirect('/app');
     }
-})->middleware(['signed'])->name('verification.verify');
-
+})->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::get('/magiclink/send', [MagicLinkLoginController::class, 'sendMagicLink'])->name('magiclink.send');
 Route::get('/magiclink/login', [MagicLinkLoginController::class, 'loginWithMagicLink'])->name('magiclink.login');
